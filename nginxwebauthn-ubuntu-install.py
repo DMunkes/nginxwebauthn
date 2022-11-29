d59e19510bd03310f0adfddb54f7c92adb4e08d2
Jan Vítek <jan.vitek@virtualmaster.com>
2022-11-29 18:08:10 +0100

    enable parallel authentications and registrations; allow user to fill in their personal info (preregistration) and send the data to an admin email

--- nginxwebauthn-ubuntu-install.py
+++ nginxwebauthn-ubuntu-install.py
@@ -2,11 +2,33 @@
 
 import os
 import sys
+import shutil
 from pathlib import Path
+
+user = sys.argv[1]
+group = sys.argv[2]
 
 print("creating directroy structure")
 Path("/opt/nginxwebauthn/credentials").mkdir(parents=True, exist_ok=True)
+Path("/opt/nginxwebauthn/registrations").mkdir(parents=True, exist_ok=True)
 Path("/opt/nginxwebauthn/headers").mkdir(parents=True, exist_ok=True)
+
+shutil.chown("/opt/nginxwebauthn/registrations", user=user, group=group)
+
+config_contents = """
+[Global]
+# after succesful token registration a form is displayed where user can fill in the token name and their personal information
+# the token is then stored in /opt/nginxwebauthn/registrations
+AllowPreregistration = false
+
+# if FromEmail and AdminEmail are configured the user personal information is sent to the AdminEmail
+#FromEmail = root@localhost
+#AdminEmail = root@localhost
+"""
+
+if not os.path.exists("/etc/nginxwebauthn.conf"):
+  with open("/etc/nginxwebauthn.conf", 'w') as f:
+    f.write(config_contents)
 
 script_path = os.path.realpath(__file__).replace("/nginxwebauthn-ubuntu-install.py", "/nginxwebauthn.py")
 
@@ -29,7 +51,7 @@ WantedBy=multi-user.target
 
 print("creating systemd service file")
 with open('/lib/systemd/system/nginxwebauthn.service', 'w') as f:
-    f.write(systemd_contents % (sys.argv[1], sys.argv[2], script_path))
+    f.write(systemd_contents % (user, group, script_path))
 
 print("running systemctl commands")
 os.system("systemctl daemon-reload")

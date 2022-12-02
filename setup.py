f538f4c34608b8c94eca89da01251ee9c80bd5b1
Jan Vítek <jan.vitek@virtualmaster.com>
2022-12-02 10:53:20 +0100

    v0.2.0 package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("readme.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='nginxwebauthn-jv',  
-     version='0.1.4',
+     version='0.2.0',
      scripts=['nginxwebauthn.py', 'nginxwebauthn-ubuntu-install.py'] ,
      author="Jan Vitek",
      author_email="mail@janvitek.com",
